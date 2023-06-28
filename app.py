20cd0b98c643883be7e762fb384a228411dd64c4
Divyanshu-Vaibhav <132071627+Divyanshu-Vaibhav@users.noreply.github.com>
2023-06-28 08:01:07 +0000

    done frontend

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
 
-app=Flask(__name__)
-Jobs=[
-  {
-    'id':1,
-    'title': 'Software Engineer',
-    'location':'Banglore,India',
-    'salary':'Rs. 10,00,000'
-  },
-   {
-    'id':2,
-    'title': 'Data Engineer',
-    'location':'Gurgaon,India',
-    'salary':'Rs. 9,00,000'
-  } ,
-{
-    'id':3,
-    'title': 'Backend Engineer',
-    'location':'Texas,US',
-    'salary':'$ 120,000'
-  }, 
-{
-    'id':4,
-    'title': 'Frontend Engineer',
-    'location':'Remote job',
-    'salary':'Rs. 6,00,000'
-  }
-]
+app = Flask(__name__)
+Jobs = [{
+  'id': 1,
+  'title': 'Software Engineer',
+  'location': 'Banglore,India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Engineer',
+  'location': 'Gurgaon,India',
+  'salary': 'Rs. 9,00,000'
+}, {
+  'id': 3,
+  'title': 'Backend Engineer',
+  'location': 'Texas,US',
+  'salary': '$ 120,000'
+}, {
+  'id': 4,
+  'title': 'Frontend Engineer',
+  'location': 'Remote job',
+  'salary': 'Rs. 6,00,000'
+}]
+
+
 @app.route("/")
 def hello():
-  return render_template('home.html',jobs=Jobs)
+  return render_template('home.html', jobs=Jobs)
 
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
- 
+@app.route("/api/jobs")
+def listjobs():
+  return jsonify(Jobs)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
