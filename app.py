b643c50b276daa177a60e802751e7d0bef296e6b
Divyanshu-Vaibhav <132071627+Divyanshu-Vaibhav@users.noreply.github.com>
2023-06-28 03:29:41 +0000

    going for brkfast

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from flask import Flask,render_template
 
 app=Flask(__name__)
+Jobs=[
+  {
+    'id':1,
+    'title': 'Software Engineer',
+    'location':'Banglore,India',
+    'salary':'Rs. 10,00,000'
+  },
+   {
+    'id':2,
+    'title': 'Data Engineer',
+    'location':'Gurgaon,India',
+    'salary':'Rs. 9,00,000'
+  } ,
+{
+    'id':3,
+    'title': 'Backend Engineer',
+    'location':'Texas,US',
+    'salary':'$ 120,000'
+  }, 
+{
+    'id':4,
+    'title': 'Frontend Engineer',
+    'location':'Remote job',
+    'salary':'Rs. 6,00,000'
+  }
+]
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',jobs=Jobs)
 
 
 if __name__=="__main__":
