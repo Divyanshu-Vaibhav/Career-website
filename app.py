88ddf1791429fdede308301295506704a9cfee25
Divyanshu-Vaibhav <132071627+Divyanshu-Vaibhav@users.noreply.github.com>
2023-06-28 02:59:49 +0000

    did the styling part

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app=Flask(__name__)
 @app.route("/")
 def hello():
-  return "Hello"
+  return render_template('home.html')
 
-print(__name__)
+
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
  
